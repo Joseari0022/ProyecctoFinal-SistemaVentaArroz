646b6626f9cafa600aac91d9c9e6d5b6f9ca52c1
Jose Ariel Paredes Gonzalez <jose_paredes2@ucne.edu.do>
2016-11-28 08:55:23 +0100

    consulta detalle

---
+++ SistemaVentaArroz/Consultas/ReportingViewerDetalleFactura.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVentaArroz.Consultas
+{
+    public partial class ReportingViewerDetalleFactura : Form
+    {
+        public ReportingViewerDetalleFactura()
+        {
+            InitializeComponent();
+        }
+
+        private void ReportingViewerDetalleFactura_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
